a2c1041140297045fb1927c80d7080512a99194c
dev.vanmit.hu <contact@vanmit.hu>
2013-11-02 18:19:34 +0000

    Per branch build using parameters.

--- README.md
+++ README.md
@@ -6,16 +6,11 @@ Allows per-branch deployment from bitbucket to a jenkins server.
 Receives an incoming POST service request from Bitbucket (https://bitbucket.org)
 and connects this through to a jenkins build.
 
-1 Install a php webserver somewhere where bitbucket can access it (e.g. mydomain.com/index.php)
-2 Install this script
+1 Install this script on a php webserver somewhere where bitbucket can access it (e.g. mydomain.com/jenbucket/index.php)
 3 Point bitbucket to this script instead of using the jenkins plugin.
-This is done by doing a post commit action (type POST).
-Set the following url as your POST url: http://mydomain.com/index.php?project_to_build=JENKINS_PROJECTNAME&branch=JENKINS_BRANCH
+This is done by doing a post commit hook action (type POST).
+Set the following url as your POST url: http://mydomain.com/jenbucket/index.php?project=JENKINS_PROJECTNAME
 4 setup the correct settings
 - $jenkins_url
 - $jenkins_token inside the index.php file.
 5 You're up and running!
-
-
-TODO:
-- input validation.
--- index.php
+++ index.php
@@ -13,51 +13,67 @@
 
 // Settings.
 //
-// The location of your jenkins deployment server. Something like
-// http:/blabla.be/job/.
-$jenkins_url = '';
+
+// The location of your jenkins deployment server. 
+$jenkins_url = 'http://localhost:8080/jenkins';
 
 // Jenkins connection token.
 $jenkins_token = '';
 
+// Validation function
+function validate($param) {
+  return !empty($param) && strlen($param) <= 128 && !preg_match('/[^a-zA-Z.-]/', $param);
+}
+
+
+// Initialization
+//
+
 // Get incoming.
-$incoming = json_decode($_POST['payload']);
+$incoming = json_decode($_POST["payload"]);
 
 // Validate build.
-if (empty($_GET['project_to_build'])) {
+if (!validate($_GET['project'])) {
   print 'No build found';
   exit();
 }
-$build_name = $_GET['project_to_build'];
+$project = $_GET['project'];
 
-// Detect if we're allowed to build.
-$build_allow = FALSE;
+// Detect branches to build
+$branches = array();
 
-if (empty($_GET['branch'])) {
-  // No branch default means build always.
-  $build_allow = TRUE;
-}
-else {
-  foreach ($incoming->commits as $commit) {
-    if ($commit->branch == $_GET['branch']) {
-      $build_allow = TRUE;
-      break;
+foreach ($incoming->commits as $commit) {
+  if (validate($commit->branch)) {
+    $branches[] = $commit->branch;
+  }
+  if (isset($commit->branches)) {
+    foreach ($commit->branches as $branch) {
+      if (validate($branch)) {
+        $branches[] = $branch;
+      }
     }
   }
 }
 
-// Only build if we're allowed to build.
-if ($build_allow) {
+
+// Start build(s)
+//
+
+foreach ($branches as $branch) {
   // Construct a request to jenkins.
-  $url = $jenkins_url . '' . $_GET['project_to_build'] . '/build?token=' . $jenkins_token;
+  $url = "$jenkins_url/job/$project/buildWithParameters?GIT_BRANCH=$branch";
+  if (!empty($jenkins_token)) {
+    $url .= "&token=$jenkins_token";
+  }
 
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, array());
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_HEADER, 1);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
+  echo "---- BUILD $url<br/>\n";
   $response = curl_exec($ch);
   curl_close($ch);
 }
